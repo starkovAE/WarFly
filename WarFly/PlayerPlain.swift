2f9fa7c532fecdf47a79da66f19eabac6888b30f
starkovAE <swix456@gmail.com>
2022-04-07 22:12:55 +0500

    10 commit. 1) Create arrays of texture. 2)Create a method planeAnimationFillArray(), it fills arrays with textures.

--- WarFly/PlayerPlain.swift
+++ WarFly/PlayerPlain.swift
@@ -15,6 +15,11 @@ class PlayerPlain: SKSpriteNode {
     var xAcceleration: CGFloat = 0
     let screenSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
     
+    //Создаем массивы текстур (для анимации полета самолета)
+    var leftTextureArrayAnimation = [SKTexture]()
+    var rightTextureArrayAnimation = [SKTexture]()
+    var forwardTextureArrayAnimation = [SKTexture]()
+    
     static func populate(at point: CGPoint) -> PlayerPlain {
         //текстура может меняться, а изображение нет
         let playerPlaneTexture = SKTexture(imageNamed: "airplane_3ver2_13")
@@ -37,12 +42,61 @@ class PlayerPlain: SKSpriteNode {
     }
     //MARK: - performFly() - выполнение полета
     func performFly() {
+        planeAnimationFillArray() //перед тем, как полетели. Нужно подгрузить текстуры
         motionManager.accelerometerUpdateInterval = 0.2 //как часто акселерометр должен обновлять ускорения
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { data, error in //data - это данные которые получим с сенсоров
             if let data = data { //если получили данные, извлекаем их в локальную дата
                 let acceleration = data.acceleration
                 self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3 //получаем данные акселерометра и немного убавляем их (делаем их не линейными, немного искажаем)
             }
+        }
+        
+    }
+    //MARK: - planeAnimationFillArray() - метод, наполнения массивов для анимации
+    private func planeAnimationFillArray() {
+        SKTextureAtlas.preloadTextureAtlases([SKTextureAtlas(named: "PlayerPlane")]) {
+        
+            self.leftTextureArrayAnimation = { //значение этого массива - это будет то, что возвращается из этого массива
+                
+                var array = [SKTexture]() //путсой массив
+                for i in stride(from: 13, through: 1, by: -1) { //перебираем от 13 до 1 с шагом - 1 (для левого поворота)
+                    let number = String(format: "%02d", i) //наше число должно быть преобразовано в строку со следующим видом
+                    let texture = SKTexture(imageNamed: "airplane_3ver2_\(number)")
+                    array.append(texture) //получился массив из текстур
+                }
+                SKTexture.preload(array, withCompletionHandler: {
+                    print("preload is done")
+                })
+                return array
+            } ()
+        
+            self.rightTextureArrayAnimation = {
+                
+                var array = [SKTexture]() //путсой массив
+                for i in stride(from: 13, through: 26, by: 1) {
+                    let number = String(format: "%02d", i) //наше число должно быть преобразовано в строку со следующим видом
+                    let texture = SKTexture(imageNamed: "airplane_3ver2_\(number)")
+                    array.append(texture) //получился массив из текстур
+                }
+                SKTexture.preload(array, withCompletionHandler: {
+                    print("preload is done")
+                })
+                return array
+            } ()
+            
+            self.forwardTextureArrayAnimation = {
+                
+                var array = [SKTexture]() //путсой массив
+                let texture = SKTexture(imageNamed: "airplane_3ver2_13")
+                array.append(texture) //получился массив из текстур
+                
+                SKTexture.preload(array, withCompletionHandler: {
+                    print("preload is done")
+                })
+                return array
+            } ()
+            
+            
         }
         
     }
