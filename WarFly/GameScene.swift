45e1566734d0acfe447c54f37547ced535e331fe
starkovAE <swix456@gmail.com>
2022-04-29 13:41:05 +0500

    25 commit. 1) Put the user interface in separate class 2) Create new method in GameSceen 3) Create object hud - HUD()

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -12,14 +12,10 @@ import GameplayKit
 class GameScene: SKScene {
 
     //создаем игрока
-var player: PlayerPlain!
-    //MARK: - Элементы пользовательского интерфейса
-    let scoreBacground = SKSpriteNode(imageNamed: "scores")
-    let scoreLabel = SKLabelNode(text: "10000")
-    let menuButton = SKSpriteNode(imageNamed: "menu")
-    let life1 = SKSpriteNode(imageNamed: "life")
-    let life2 = SKSpriteNode(imageNamed: "life")
-    let life3 = SKSpriteNode(imageNamed: "life")
+    private var player: PlayerPlain!
+    private  let hud = HUD()
+    private let screenSize = UIScreen.main.bounds.size
+   
     //MARK: - didMove(to View:)
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
@@ -34,39 +30,11 @@ var player: PlayerPlain!
         
         spawnPowerUp()
         spawnEmenies()
-        configureUI()
+        createHUD()
     }
-    //MARK: - configureUI()
-    private func configureUI() {
-        scoreBacground.position = CGPoint(x: scoreBacground.size.width + 10, y: self.size.height - scoreBacground.size.height / 2 - 25)
-        scoreBacground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
-        scoreBacground.zPosition = 99
-        addChild(scoreBacground)
-        
-        scoreLabel.horizontalAlignmentMode = .right // выравнивание по горизонтали
-        scoreLabel.verticalAlignmentMode = .center //по вертикали
-        scoreLabel.position = CGPoint(x: -10, y: 3)
-        scoreLabel.zPosition = 100
-        scoreLabel.fontName = "AmericanTypewriter-Bold"
-        scoreLabel.fontSize = 30
-        scoreBacground.addChild(scoreLabel)//помещаем на scoreBacground
-        
-        menuButton.position = CGPoint(x: 20, y: 20)
-        menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-        menuButton.zPosition = 100
-        addChild(menuButton)
-        
-        let lifes = [life1,life2,life3]
-        for (index, life) in lifes.enumerated() { //enumerated() - возращает не только элемент массива но  и его индекс
-            life.position = CGPoint(x: self.size.width - CGFloat(index + 1) * (life.size.width + 3), y: 20)
-            life.zPosition = 100
-            life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-            addChild(life)
-        }
-        
-        
-        
-        
+    private func createHUD() {
+        addChild(hud)
+        hud.configureUI(screenSize: screenSize)
     }
     //MARK: - createPowerUp()
     private func spawnPowerUp() {
